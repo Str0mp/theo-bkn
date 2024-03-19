24f500497e8c6f140526831566681939c18428b2
Jorge Martini <jorge.martini@icloud.com>
2024-03-18 21:22:03 -0300

    test trigger

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>El mati es weko</h1>
+      <h1>El mati es muy weko</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           El contador es {count}
